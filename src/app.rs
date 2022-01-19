3a75bcd2a56eb272802a49432a9e862da499a770
Mike Shearer <shearerbeard@gmail.com>
2022-01-19 15:19:03 -0700

    1st pass - this proto just pulls in components from the cdn - next pass I see examples on yew-paternfly for handling local javascript libs

--- src/app.rs
+++ src/app.rs
@@ -7,6 +7,21 @@ pub fn app() -> Html {
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
             <h1>{ "Hello World!" }</h1>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <p>{ "Lets do some more!" }</p>
+            <div>
+                <paper-button raised={"true"}><iron-icon icon="check"></iron-icon>{ "OK" }</paper-button>
+                <paper-button raised={"true"}><iron-icon icon="clear"></iron-icon>{ "CANCEL" }</paper-button>
+            </div>
+
+            <p><paper-checkbox>{ "Checkbox" }</paper-checkbox></p>
+
+            <p><paper-toggle-button></paper-toggle-button></p>
+
+            <paper-tabs selected={ true }>
+            <paper-tab>{ "TAB 1" }</paper-tab>
+            <paper-tab>{ "TAB 2" }</paper-tab>
+            <paper-tab>{ "TAB 3" }</paper-tab>
+            </paper-tabs>
         </main>
     }
 }
