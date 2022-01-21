6250eb53dc970f33576d395a1ebccd2d08925ea0
Mike Shearer <shearerbeard@gmail.com>
2022-01-21 10:43:56 -0700

    use js modules to get my web components -> add rollupjs

--- src/app.rs
+++ src/app.rs
@@ -8,20 +8,7 @@ pub fn app() -> Html {
             <h1>{ "Hello World!" }</h1>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
             <p>{ "Lets do some more!" }</p>
-            <div>
-                <paper-button raised={"true"}><iron-icon icon="check"></iron-icon>{ "OK" }</paper-button>
-                <paper-button raised={"true"}><iron-icon icon="clear"></iron-icon>{ "CANCEL" }</paper-button>
-            </div>
-
-            <p><paper-checkbox>{ "Checkbox" }</paper-checkbox></p>
-
-            <p><paper-toggle-button></paper-toggle-button></p>
-
-            <paper-tabs selected={ true }>
-            <paper-tab>{ "TAB 1" }</paper-tab>
-            <paper-tab>{ "TAB 2" }</paper-tab>
-            <paper-tab>{ "TAB 3" }</paper-tab>
-            </paper-tabs>
+            <mwc-button label="Add 1" raised="true" icon="code"></mwc-button>
         </main>
     }
 }
